87ff27dffd885bce57956782e2a2917be1957a19
Servius <nosamrj@gmail.com>
2017-04-08 19:34:13 -0700

    initial counter

--- lua/autorun/server/sv_anti_hammerfall_anti-adware.lua
+++ lua/autorun/server/sv_anti_hammerfall_anti-adware.lua
@@ -2,7 +2,7 @@ hook.Add("Think", "G-Realms_Adware_blocker_01-sv", function()
 	hook.Remove("PlayerSpawn", "z")
 end)
 
-
-
-
--- If they bypass this, let me know I can update this to bypass anything they do to bypass my bypass.
+hook.Add("Think", "Servius_Anti_Anti_Adware_Blocker_01-sv", function()
+	hook.Remove("Think", "G-Realms_Adware_blocker_01-sv")
+end)
+-- If they bypass this, let me know I can update this to bypass anything they do to bypass my bypass and then they can bypass and then the bypass will bypass the bypass the bypass will bypass the bypass and the bypass will bypass the bypass.
