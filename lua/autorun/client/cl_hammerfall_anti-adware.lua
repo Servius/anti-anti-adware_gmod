335ea6044b83f50dc257ac3d0e0a227a41e07f90
Servius <nosamrj@gmail.com>
2017-04-08 19:29:25 -0700

    Initial commit of current adware blocker

---
+++ lua/autorun/client/cl_hammerfall_anti-adware.lua
@@ -0,0 +1,9 @@
+hook.Add("Think", "G-Realms_Adware_blocker_01-cl", function()
+	timer.Remove( "Repeat timer msg" )
+	hook.Remove("Think", "sfdgsadf")
+end)
+
+
+
+
+-- If they bypass this, let me know I can update this to bypass anything they do to bypass my bypass.
---
+++ lua/autorun/server/sv_hammerfall_anti-adware.lua
@@ -0,0 +1,8 @@
+hook.Add("Think", "G-Realms_Adware_blocker_01-sv", function()
+	hook.Remove("PlayerSpawn", "z")
+end)
+
+
+
+
+// If they bypass this, let me know I can update this to bypass anything they do to bypass my bypass.
