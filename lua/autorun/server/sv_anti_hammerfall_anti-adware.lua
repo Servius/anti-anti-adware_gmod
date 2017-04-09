2bfe026cd3ab135087240e1c3b20c3434082f800
Servius <nosamrj@gmail.com>
2017-04-08 19:31:15 -0700

    What is this the stone age; Better comment out

--- lua/autorun/server/sv_anti_hammerfall_anti-adware.lua
+++ lua/autorun/server/sv_anti_hammerfall_anti-adware.lua
@@ -5,4 +5,4 @@ end)
 
 
 
-// If they bypass this, let me know I can update this to bypass anything they do to bypass my bypass.
+-- If they bypass this, let me know I can update this to bypass anything they do to bypass my bypass.
